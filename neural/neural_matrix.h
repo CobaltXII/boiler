7de2e7f3dcf8357e5b47d35c5c1ffd84bf9d8623
CobaltXII <sidatt64@gmail.com>
2018-10-04 14:26:19 -0400

    Added empty neural_matrix header

---
+++ neural/neural_matrix.h
@@ -0,0 +1,36 @@
+#pragma once
+
+std::random_device r_device;
+
+// A matrix with a variable (positive) number of rows and columns.
+
+struct matrix
+{
+	unsigned int r_C;
+	unsigned int c_C;
+
+	// Remember, this vector contains a vector of rows. The rows contain values. When we access 
+	// this vector, the syntax is u_M[row][column].
+
+	std::vector<std::vector<double>> u_M;
+
+	matrix(unsigned int _r_C, unsigned int _c_C)
+	{
+		r_C = _r_C;
+		c_C = _c_C;
+
+		// Initialize empty matrix.
+
+		for (int i = 0; i < r_C; i++)
+		{
+			std::vector<double> _R;
+
+			for (int j = 0; j < c_C; j++)
+			{
+				_R.push_back(0.0);
+			}
+
+			u_M.push_back(_R);
+		}
+	}
+};
