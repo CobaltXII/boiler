e7cccc03333751bfd4151f1d0ff0ee1b3a88dcfb
CobaltXII <sidatt64@gmail.com>
2018-10-05 11:28:40 -0400

    Added learning rate constructor

--- neural/neural_network.h
+++ neural/neural_network.h
@@ -42,4 +42,32 @@ struct neural_network
 
 		l_R = 0.1;
 	}
+
+	neural_network
+	(
+		unsigned int _I,
+		unsigned int _H,
+		unsigned int _O,
+
+		double _l_R
+	)
+	{
+		i_nodes = _I;
+		h_nodes = _H;
+		o_nodes = _O;
+
+		ih_w = matrix(_H, _I);
+		ho_w = matrix(_O, _H);
+
+		ih_w.randomize();
+		ho_w.randomize();
+
+		h_b = matrix(_H, 1);
+		o_b = matrix(_O, 1);
+
+		h_b.randomize();
+		o_b.randomize();
+
+		l_R = _l_R;
+	}
 };
