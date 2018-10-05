384116753ef1504eaec6595d2d9a54e45105bce9
CobaltXII <sidatt64@gmail.com>
2018-10-05 11:27:37 -0400

    Added weight matrices

--- neural/neural_network.h
+++ neural/neural_network.h
@@ -1,11 +1,14 @@
 #pragma once
 
 // A neural network with a variable (positive) and constant after created amount of input, hidden
-// and output nodes. Uses the Sigmoid activation function.
+// and output nodes.
 
 struct neural_network
 {
 	unsigned int i_nodes;
 	unsigned int h_nodes;
 	unsigned int o_nodes;
+
+	matrix ih_w;
+	matrix ho_w;
 };
