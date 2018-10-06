cbca65134b35de031445f12ad9531429d7efc5c7
CobaltXII <sidatt64@gmail.com>
2018-10-06 14:39:52 -0400

    stb_image.h renamed to stb_image.hpp

--- boiler/boiler.h
+++ boiler/boiler.h
@@ -25,7 +25,7 @@
 // such as SDL_image, or using difficult, massive APIs like libpng. Thanks to Sean T. Barrett and
 // other contributors.
 
-#include "stb_image.h"
+#include "stb_image.hpp"
 
 #endif
 
