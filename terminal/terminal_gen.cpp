7e73caa3cbded09aa5393bba485178ae7d9a8946
CobaltXII <sidatt64@gmail.com>
2018-10-12 19:15:55 -0400

    Array size is now generated to avoid warnings

--- terminal/terminal_gen.cpp
+++ terminal/terminal_gen.cpp
@@ -30,7 +30,7 @@ struct game: boiler
 
 		std::stringstream __437__o;
 
-		__437__o << "unsigned char __437__ =\n{\n";
+		__437__o << "unsigned char __437__[" << __437__w * __437__h << "] =\n{\n";
 
 		for (int y = 0; y < __437__h; y++)
 		{
