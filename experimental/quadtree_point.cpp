87b3ac4b3f1db1e681aeb152e40d7bb580fdf4f1
CobaltXII <sidatt64@gmail.com>
2018-11-10 20:34:05 -0500

    Started work on a quadtree class

--- experimental/quadtree_point.cpp
+++ experimental/quadtree_point.cpp
@@ -17,3 +17,20 @@ struct point
 		y = _y;
 	}
 };
+
+// It's a dirty quadtree who does not clean up after himself.
+
+struct quadtree
+{
+	quadtree* tl;
+	quadtree* tr;
+	quadtree* bl;
+	quadtree* br;
+
+	point* child;
+
+	double x;
+	double y;
+
+	double w;
+	double h;
