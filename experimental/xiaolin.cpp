111a666d6400a3476dbd3586512316f8f739ef35
CobaltXII <sidatt64@gmail.com>
2018-11-16 16:20:49 -0500

    Added thick line rasterization function

--- experimental/xiaolin.cpp
+++ experimental/xiaolin.cpp
@@ -93,6 +93,69 @@ struct game: boiler
 		}
 	}
 
+	void xiaolin_thick_line(int x0, int y0, int x1, int y1, float wd, unsigned int c)
+	{ 
+		int dx = abs(x1 - x0);
+
+		int sx = x0 < x1 ? 1 : -1;
+
+		int dy = abs(y1 - y0);
+
+		int sy = y0 < y1 ? 1 : -1;
+
+		int err = dx - dy;
+
+		int e2;
+
+		int x2;
+		int y2;
+
+		float ed = dx + dy == 0 ? 1 : sqrt((float)dx * dx + (float)dy * dy);
+
+		for (wd = (wd + 1) / 2;;) 
+		{
+			plot_aa(x0, y0, c, fmax(0, 255 * (abs(err -dx + dy) / ed - wd + 1)));
+
+			e2 = err; 
+
+			x2 = x0;
+			
+			if (2 * e2 >= -dx) 
+			{
+				for (e2 += dy, y2 = y0; e2 < ed * wd && (y1 != y2 || dx > dy); e2 += dx)
+				
+				plot_aa(x0, y2 += sy, c, fmax(0, 255 * (abs(e2) / ed - wd + 1)));
+				
+				if (x0 == x1)
+				{
+					break;
+				}
+				
+				e2 = err; 
+
+				err -= dy; 
+
+				x0 += sx; 
+			} 
+
+			if (2 * e2 <= dy) 
+			{
+				for (e2 = dx - e2; e2 < ed * wd && (x1 != x2 || dx < dy); e2 += dy)
+				
+				plot_aa(x2 += sx, y0, c, fmax(0, 255 * (abs(e2) / ed - wd + 1)));
+				
+				if (y0 == y1)
+				{
+					break;
+				}
+				
+				err += dx; 
+
+				y0 += sy; 
+			}
+		}
+	}
+
 	void steam() override
 	{
 		width = 800;
