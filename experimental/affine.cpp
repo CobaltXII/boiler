dc291dd5eba75c708f869946ec0ae96fb9670401
CobaltXII <sidatt64@gmail.com>
2018-11-17 17:56:11 -0500

    Added identity matrix generator

--- experimental/affine.cpp
+++ experimental/affine.cpp
@@ -12,3 +12,22 @@ struct mat3
 {
 	double m[3][3];
 };
+
+mat3 mat_identity()
+{
+	mat3 m;
+
+	for (int x = 0; x < 3; x++)
+	{
+		for (int y = 0; y < 3; y++)
+		{
+			m.m[x][y] = 0.0;
+		}
+	}
+
+	m.m[0][0] = 1.0;
+	m.m[1][1] = 1.0;
+	m.m[2][2] = 1.0;
+
+	return m;
+}
