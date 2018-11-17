0cca3b88be471d156515cf026b72503c4c9ad585
CobaltXII <sidatt64@gmail.com>
2018-11-17 17:55:57 -0500

    Added matrix class

---
+++ experimental/affine.cpp
@@ -0,0 +1,14 @@
+// Affine transformations? Awesome.
+
+#define BOIL_USE_STB_IMAGE 
+
+#include "../boiler/boiler.h"
+
+#include <vector>
+#include <utility>
+#include <iostream>
+
+struct mat3
+{
+	double m[3][3];
+};
