241026f97c8955ac8cb5e29447cd049d444f0087
CobaltXII <sidatt64@gmail.com>
2018-11-17 17:56:38 -0500

    Added scalar matrix generator

--- experimental/affine.cpp
+++ experimental/affine.cpp
@@ -75,3 +75,23 @@ mat3 mat_rotate(double theta)
 
 	return m;
 }
+
+mat3 mat_scalar(double sx, double sy)
+{
+	mat3 m;
+
+	for (int x = 0; x < 3; x++)
+	{
+		for (int y = 0; y < 3; y++)
+		{
+			m.m[x][y] = 0.0;
+		}
+	}
+
+	m.m[0][0] = sx;
+	m.m[1][1] = sy;
+
+	m.m[2][2] = 1.0;
+
+	return m;
+}
