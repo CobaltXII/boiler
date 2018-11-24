f57b84aa45ccac9a09de606e045a4c346ac3eb45
CobaltXII <sidatt64@gmail.com>
2018-11-24 11:14:53 -0500

    Implemented vector-vector cross product

--- gl/gl.cpp
+++ gl/gl.cpp
@@ -118,3 +118,16 @@ vec3 normalize(vec3 i)
 
 	return o;
 }
+
+// Cross product.
+
+vec3 cross(vec3 v1, vec3 v2)
+{
+	vec3 v;
+
+	v.x = v1.y * v2.z - v1.z * v2.y;
+	v.y = v1.z * v2.x - v1.x * v2.z;
+	v.z = v1.x * v2.y - v1.y * v2.x;
+
+	return v;
+}
