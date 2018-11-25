576462852205066083ca4542672ef34573761a63
CobaltXII <sidatt64@gmail.com>
2018-11-25 12:40:07 -0500

    Added vector/matrix multiplication function

--- gl/gl.cpp
+++ gl/gl.cpp
@@ -578,3 +578,25 @@ mat4 mat_inverse(mat4 m)
 
 	return matrix;
 }
+
+// Multiply vector by matrix.
+
+vec3 multiply(vec3 i, mat4 m)
+{
+	vec3 o;
+
+	o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+	o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+	o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+
+	float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+	if (w != 0.0)
+	{
+		o.x /= w; 
+		o.y /= w; 
+		o.z /= w;
+	}
+
+	return o;
+}
