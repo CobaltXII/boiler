53bbe942420545373c0be6a7291127671e87347b
CobaltXII <sidatt64@gmail.com>
2018-11-25 12:39:52 -0500

    Added matrix pseudo-inverse function

--- gl/gl.cpp
+++ gl/gl.cpp
@@ -547,3 +547,34 @@ mat4 mat_point_at(vec3 pos, vec3 target, vec3 up)
 
 	return matrix;
 }
+
+// Invert matrix, only works on some matrices?
+
+mat4 mat_inverse(mat4 m)
+{
+	mat4 matrix;
+
+	matrix.m[0][0] = m.m[0][0]; 
+	matrix.m[0][1] = m.m[1][0]; 
+	matrix.m[0][2] = m.m[2][0]; 
+
+	matrix.m[1][0] = m.m[0][1]; 
+	matrix.m[1][1] = m.m[1][1]; 
+	matrix.m[1][2] = m.m[2][1]; 
+
+	matrix.m[2][0] = m.m[0][2]; 
+	matrix.m[2][1] = m.m[1][2]; 
+	matrix.m[2][2] = m.m[2][2]; 
+	
+	matrix.m[3][0] = -(m.m[3][0] * matrix.m[0][0] + m.m[3][1] * matrix.m[1][0] + m.m[3][2] * matrix.m[2][0]);
+	matrix.m[3][1] = -(m.m[3][0] * matrix.m[0][1] + m.m[3][1] * matrix.m[1][1] + m.m[3][2] * matrix.m[2][1]);
+	matrix.m[3][2] = -(m.m[3][0] * matrix.m[0][2] + m.m[3][1] * matrix.m[1][2] + m.m[3][2] * matrix.m[2][2]);
+
+	matrix.m[0][3] = 0.0;
+	matrix.m[1][3] = 0.0;
+	matrix.m[2][3] = 0.0;
+
+	matrix.m[3][3] = 1.0;
+
+	return matrix;
+}
