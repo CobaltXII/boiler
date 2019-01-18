3b6e34bc47fe139f83255a9793528402c3d0fa4b
CobaltXII <sidatt64@gmail.com>
2019-01-17 20:05:06 -0500

    Implemented inline point random_normal

--- optics/point.hpp
+++ optics/point.hpp
@@ -31,3 +31,17 @@ inline point normalize(point victim)
 
 	return victim;
 }
+
+// Calculate a random normal.
+
+inline point random_normal()
+{
+	real angle = real(rand()) / real(RAND_MAX) * 2.0f * M_PI;
+
+	point out;
+
+	out.x = cos(angle);
+	out.y = sin(angle);
+
+	return out;
+}
