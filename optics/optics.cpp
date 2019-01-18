1b4ea4acce67c30f17baa8885d83a3bf40592056
CobaltXII <sidatt64@gmail.com>
2019-01-17 20:07:46 -0500

    Implemented typedef float real

---
+++ optics/optics.cpp
@@ -0,0 +1,12 @@
+// Optics? Awesome.
+
+#include "../boiler/boiler.h"
+
+#include <vector>
+#include <utility>
+#include <iostream>
+
+// Use 'real' instead of float or double. This makes it easy to switch between
+// floating-point precisions.
+
+typedef float real;
