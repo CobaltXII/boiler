3794882bd45c044d90cfcafd095937fdd44bf6ce
CobaltXII <sidatt64@gmail.com>
2019-01-17 20:04:55 -0500

    Implemented inline point normalize

--- optics/point.hpp
+++ optics/point.hpp
@@ -19,3 +19,15 @@ struct point
 		y = _y;
 	}
 };
+
+// Normalize a point.
+
+inline point normalize(point victim)
+{
+	real l = sqrt(victim.x * victim.x + victim.y * victim.y);
+
+	victim.x = victim.x / l;
+	victim.y = victim.y / l;
+
+	return victim;
+}
