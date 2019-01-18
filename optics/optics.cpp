00273cab3e9064e7c15f1f9d7a99e803510be3a8
CobaltXII <sidatt64@gmail.com>
2019-01-17 20:08:58 -0500

    Implemented int main

--- optics/optics.cpp
+++ optics/optics.cpp
@@ -10,3 +10,59 @@
 // floating-point precisions.
 
 typedef float real;
+
+#include "point.hpp"
+
+#include "intersectable.hpp"
+
+#include "reflective_segment.hpp"
+
+#include "refractive_segment.hpp"
+
+#include "strobe_filter_segment.hpp"
+
+#include "subtractive_filter_segment.hpp"
+
+#include "emitter.hpp"
+
+#include "line.hpp"
+
+// Boiler game class.
+
+struct game: boiler
+{	
+	// The side length of a grid square.
+
+	const real grid = 8.0f;
+
+	// The intersectable objects in the scene.
+
+	std::vector<intersectable*> scene_intersectable;
+
+	// The emitters in the scene.
+
+	std::vector<emitter*> scene_emitter;
+
+	// The point being dragged.
+
+	point* dragged = nullptr;
+};
+
+// Entry point for the software renderer.
+
+int main(int argc, char** argv)
+{
+	game demo;
+
+	if (demo.make() != 0)
+	{
+		std::cout << "Could not initialize Boiler." << std::endl;
+
+		return 1;
+	}
+
+	demo.engine();
+	demo.sweep();
+
+	return 0;
+}
