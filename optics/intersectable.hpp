8161bd3dfc158583647093ba5b6cfd9bc6f201a6
CobaltXII <sidatt64@gmail.com>
2019-01-17 20:05:21 -0500

    Implemented enum intersectable_type

---
+++ optics/intersectable.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// All intersectable types.
+
+enum intersectable_type
+{
+	intersectable_reflective_segment,
+
+	intersectable_refractive_segment,
+
+	intersectable_strobe_filter_segment,
+
+	intersectable_subtractive_filter_segment
+};
