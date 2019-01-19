bde90df2574e3a554ba346f44ba178ab75e93634
CobaltXII <sidatt64@gmail.com>
2019-01-18 19:09:38 -0500

    Implemented struct reflective_circle

---
+++ optics/reflective_circle.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+// A reflective circle.
+
+struct reflective_circle: public intersectable
+{
+	// Old position.
+
+	point op;
+
+	// Position.
+
+	point p;
+
+	// Radius.
+
+	real r;
+
+	// Old radius anchor.
+
+	point oa;
+
+	// Radius anchor.
+
+	point a;
+
+	// Recalculate after moving.
+
+	inline void recalculate()
+	{
+		op = p;
+
+		oa = a;
+
+		r = a.x - p.x;
+
+		a.x = p.x + r;
+
+		a.y = p.y;
+	}
+
+	// Check for changes.
+
+	inline bool changed()
+	{
+		return op.x != p.x || op.y != p.y || oa.x != a.x || oa.y != a.y;
+	}
+
+	reflective_circle(point _p, real _r)
+	{
+		type = intersectable_reflective_circle;
+
+		p = _p;
+
+		r = _r;
+
+		a = point(p.x + r, p.y);
+
+		op = p;
+
+		oa = a;
+	}
+};
