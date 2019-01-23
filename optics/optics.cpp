0f374f45f248254d96c69d275e3195304f2c2091
CobaltXII <sidatt64@gmail.com>
2019-01-23 15:09:15 -0500

    Added reflective segment drop

--- optics/optics.cpp
+++ optics/optics.cpp
@@ -1237,6 +1237,16 @@ struct game: boiler
 			{
 				scene_intersectable.push_back(new refractive_circle(point(round(mouse_x / grid) * grid, round(mouse_y / grid) * grid), 48.0f));
 			}
+			else if (GUI.status_text == "Press SPACE to drop a Reflective Segment")
+			{
+				real rx1 = round(mouse_x / grid) * grid - (grid * 3);
+				real rx2 = round(mouse_x / grid) * grid + (grid * 3);
+
+				real ry1 = round(mouse_y / grid) * grid;
+				real ry2 = round(mouse_y / grid) * grid;
+
+				scene_intersectable.push_back(new reflective_segment(point(rx1, ry1), point(rx2, ry2)));
+			}
 		{
 			linergb(0, y, width, y, grid_color);
 		}
