7dc168569f08c7b52ef8fdfaa64d3986ce50975b
CobaltXII <sidatt64@gmail.com>
2019-01-23 15:12:25 -0500

    Added Enable Auto Cycle callback

--- optics/optics.cpp
+++ optics/optics.cpp
@@ -1560,6 +1560,14 @@ int menu_item_callback(void* userdata, std::string label, int tab, int index)
 
 		return DOS_MIR_HIDE_LOCK;
 	}
+	else if (label == "Enable Auto Cycle")
+	{
+		parent->GUI.status_text = "Click on the Emitter to enable Auto Cycle on";
+
+		parent->state = gs_place;
+
+		return DOS_MIR_HIDE_LOCK;
+	}
 // Entry point for the software renderer.
 
 int main(int argc, char** argv)
