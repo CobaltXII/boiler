1f462fcba1b37c6efa555d712e2bb96af0e42cdc
CobaltXII <sidatt64@gmail.com>
2019-01-23 15:13:35 -0500

    Added Add Reflective Segment callback

--- optics/optics.cpp
+++ optics/optics.cpp
@@ -1642,6 +1642,14 @@ int menu_item_callback(void* userdata, std::string label, int tab, int index)
 		return DOS_MIR_HIDE_LOCK;
 	}
 
+	if (label == "Add Reflective Segment")
+	{
+		parent->GUI.status_text = "Press SPACE to drop a Reflective Segment";
+
+		parent->state = gs_place;
+
+		return DOS_MIR_HIDE_LOCK;
+	}
 // Entry point for the software renderer.
 
 int main(int argc, char** argv)
