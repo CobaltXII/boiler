a0fe855811707256cb4a8e06b01409a0945c6149
CobaltXII <sidatt64@gmail.com>
2019-01-25 12:14:21 -0500

    Implemented struct constraint

--- experimental/verlet.cpp
+++ experimental/verlet.cpp
@@ -59,3 +59,47 @@ struct point
 		locked = _locked;
 	}
 };
+
+// Rigid constraint.
+
+struct constraint
+{
+	// Connected points.
+
+	point* p1;
+	point* p2;
+
+	// Distance to maintain.
+
+	real d;
+
+	// Inactive constraints are invisible and have no effect on their 
+	// contained points.
+
+	bool active;
+
+	// Default constructor.
+
+	constraint
+	(
+		point* _p1,
+		point* _p2,
+
+		bool _active = true
+	)
+	{
+		p1 = _p1;
+		p2 = _p2;
+
+		real dx = p2->x - p1->x;
+		real dy = p2->y - p1->y;
+
+		d = sqrtf
+		(
+			dx * dx +
+			dy * dy
+		);
+
+		active = _active;
+	}
+};
