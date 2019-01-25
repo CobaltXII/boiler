296f10145d5f4ceaab3c11afc92bc17ecbea514c
CobaltXII <sidatt64@gmail.com>
2019-01-25 12:13:36 -0500

    Implemented real clampm

--- experimental/verlet.cpp
+++ experimental/verlet.cpp
@@ -7,3 +7,17 @@
 #include <iostream>
 
 typedef float real;
+
+// Naive velocity clamp.
+
+real clampm(real val, real clamp)
+{
+	if (val > 0.0f)
+	{
+		return std::min(val, clamp);
+	}
+	else
+	{
+		return std::max(val, -clamp);
+	}
+}
