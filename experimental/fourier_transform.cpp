91271c56d9588c215be7014063788f9f02f53024
CobaltXII <sidatt64@gmail.com>
2019-01-26 18:44:04 -0500

    Implemented std::vector<fourier_t> discrete_fourier_transform

--- experimental/fourier_transform.cpp
+++ experimental/fourier_transform.cpp
@@ -18,3 +18,33 @@ struct fourier_t
 
 	float phase;
 };
+
+// Discrete Fourier transform.
+
+std::vector<fourier_t> discrete_fourier_transform(std::vector<float> x)
+{
+	float N = x.size();
+
+	std::vector<fourier_t> X(N);
+
+	for (float k = 0.0f; k < N; k++)
+	{
+		float re = 0.0f;
+		float im = 0.0f;
+
+		for (float n = 0.0f; n < N; n++)
+		{
+			float phi = (M_PI * 2.0f * k * n) / N;
+
+			re = re + x[int(n)] * cos(phi);
+			im = im - x[int(n)] * sin(phi);
+		}
+
+		re = re / N;
+		im = im / N;
+
+		X[int(k)] = {re, im, k, sqrt(re * re + im * im), atan2(im, re)};
+	}
+
+	return X;
+}
