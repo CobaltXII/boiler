c947230f8c3cb6e0bb3fff9dd42d276a5f2614b9
CobaltXII <sidatt64@gmail.com>
2019-01-26 18:43:52 -0500

    Implemented struct fourier_t

---
+++ experimental/fourier_transform.cpp
@@ -0,0 +1,20 @@
+// Fourier transform? Awesome.
+
+#include "../boiler/boiler.h"
+
+#include <vector>
+#include <utility>
+#include <sstream>
+#include <iostream>
+
+struct fourier_t
+{
+	float re;
+	float im;
+
+	float freq;
+
+	float amp;
+
+	float phase;
+};
