c6db55a442c2114701a53321425223fb86360369
CobaltXII <sidatt64@gmail.com>
2019-01-28 11:03:34 -0500

    Implemented std::vector<point> k_means

--- experimental/k_means.cpp
+++ experimental/k_means.cpp
@@ -23,3 +23,89 @@ struct point
 		y = _y;
 	}
 };
+
+std::vector<point> k_means(std::vector<point>& dataset, int k, int j)
+{
+	// 'k' cluster locations.
+
+	float cx[k];
+	float cy[k];
+
+	// Start at random positions.
+
+	for (int i = 0; i < k; i++)
+	{
+		cx[i] = rand_01();
+		cy[i] = rand_01();
+	}
+
+	// Iterate 'j' times.
+
+	for (int i = 0; i < j; i++)
+	{
+		// 'k' cluster buckets.
+
+		std::vector<point*> buckets[k];
+
+		// Assign each point in the dataset to a bucket depending on which 
+		// cluster the point is closest to.
+
+		for (int m = 0; m < dataset.size(); m++)
+		{
+			float max_dist = INFINITY;
+
+			int index = -1;
+
+			for (int n = 0; n < k; n++)
+			{
+				float dx = dataset[m].x - cx[n];
+				float dy = dataset[m].y - cy[n];
+
+				float d = dx * dx + dy * dy;
+
+				if (d < max_dist)
+				{
+					max_dist = d;
+
+					index = n;
+				}
+			}
+
+			buckets[index].push_back(&(dataset[m]));
+		}
+
+		// Move each cluster to the average position of it's bucket's 
+		// contents.
+
+		for (int m = 0; m < k; m++)
+		{
+			float avgx = 0.0f;
+			float avgy = 0.0f;
+
+			for (int n = 0; n < buckets[m].size(); n++)
+			{
+				avgx += buckets[m][n]->x;
+				avgy += buckets[m][n]->y;
+			}
+
+			cx[m] = avgx / float(buckets[m].size());
+			cy[m] = avgy / float(buckets[m].size());
+		}
+	}
+
+	// Construct a vector of clusters to return.
+
+	std::vector<point> clusters;
+
+	for (int i = 0; i < k; i++)
+	{
+		point c;
+
+		c.x = cx[i];
+		c.y = cy[i];
+
+		clusters.push_back(c);
+	}
+
+	return clusters;
+}
