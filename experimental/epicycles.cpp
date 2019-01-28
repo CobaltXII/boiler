14820547d9cd021ded313d5ac769afcb5af36474
CobaltXII <sidatt64@gmail.com>
2019-01-28 17:17:37 -0500

    Implemented std::vector<fourier_t> discrete_fourier_transform

--- experimental/epicycles.cpp
+++ experimental/epicycles.cpp
@@ -40,3 +40,33 @@ struct point
 		y = _y;
 	}
 };
+
+// Discrete Fourier transform.
+
+std::vector<fourier_t> discrete_fourier_transform(std::vector<point>& x)
+{
+	float N = x.size();
+
+	std::vector<fourier_t> X(N);
+
+	for (float k = 0.0f; k < N; k++)
+	{
+		float re = 0.0f;
+		float im = 0.0f;
+
+		for (float n = 0.0f; n < N; n++)
+		{
+			float phi = (M_PI * 2.0f * k * n) / N;
+
+			re = re + x[int(n)].x * cos(phi) + x[int(n)].y * sin(phi);
+			im = im - x[int(n)].x * sin(phi) + x[int(n)].y * cos(phi);
+		}
+
+		re = re / N;
+		im = im / N;
+
+		X[int(k)] = {re, im, k, sqrt(re * re + im * im), atan2(im, re)};
+	}
+
+	return X;
+}
