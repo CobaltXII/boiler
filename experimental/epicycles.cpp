e9232fd78bebc85bdcfbb5206dce066501e3dbab
CobaltXII <sidatt64@gmail.com>
2019-01-28 17:18:17 -0500

    Added an epicycle drawing machine

--- experimental/epicycles.cpp
+++ experimental/epicycles.cpp
@@ -103,6 +103,110 @@ struct game: boiler
 
 		std::cout << "Press the mouse down to begin drawing a continuous path. Once you release the mouse, the epicycles will draw your path back to you." << std::endl;
 	}
+
+	void draw() override
+	{
+		black();
+
+		if (state == 0)
+		{
+			if (mouse_l)
+			{
+				// Mouse was pressed, begin capturing input.
+
+				state = 1;
+			}
+		}
+		else if (state == 1)
+		{
+			if (mouse_l)
+			{
+				// Mouse is still being pressed, continue capturing input. 
+				// Store this input in the input vector.
+
+				input.push_back(point(mouse_x, mouse_y));
+			}
+			else
+			{
+				// Mouse was released, conclude capturing input. Do the 
+				// Fourier transform on the captured input.
+
+				state = 2;
+
+				std::vector<point> fourier_input;
+
+				for (int i = 0; i < input.size(); i++)
+				{
+					point datum;
+
+					datum.x = input[i].x - cx;
+					datum.y = input[i].y - cy;
+
+					fourier_input.push_back(datum);
+				}
+
+				fourier = discrete_fourier_transform(fourier_input);
+			}
+
+			// Draw the currently captured input.
+
+			for (int i = 0; i < input.size() - 1; i++)
+			{
+				linergb
+				(
+					input[i].x,
+					input[i].y,
+
+					input[i + 1].x,
+					input[i + 1].y,
+
+					rgb(255, 255, 255)
+				);
+			}
+		}
+		else if (state == 2)
+		{
+			// Get the time.
+
+			float t = iteration * (2.0f * M_PI) / float(fourier.size());
+
+			// Draw the fractal spirograph.
+
+			float x = cx;
+			float y = cy;
+
+			for (int i = 0; i < fourier.size(); i++)
+			{
+				float px = x;
+				float py = y;
+
+	    		float radius = fourier[i].amp;
+
+	    		x += radius * cos(fourier[i].freq * t + fourier[i].phase);
+	    		y += radius * sin(fourier[i].freq * t + fourier[i].phase);
+
+	    		circlergb(px, py, radius, rgb(63, 63, 63));
+
+	    		linergb(px, py, x, y, rgb(127, 127, 127));
+			}
+
+			// Draw linex and liney.
+
+			linex.push_back(x);
+			liney.push_back(y);
+
+			for (int i = 0; i < linex.size() - 1; i++)
+			{
+				linergb(linex[i], liney[i], linex[i + 1], liney[i + 1], rgb(255, 255, 255));
+			}
+
+			if (linex.size() > fourier.size() * 16)
+			{
+				linex.erase(linex.begin());
+				liney.erase(liney.begin());
+			}
+		}
+	}
 };
 
 // Entry point for the software renderer.
