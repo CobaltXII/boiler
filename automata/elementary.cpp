6df6db45f0ad147ccb85abbbe0014cbc84084a04
CobaltXII <sidatt64@gmail.com>
2019-01-31 14:39:18 -0500

    Implemented typedef unsigned char cell

---
+++ automata/elementary.cpp
@@ -0,0 +1,11 @@
+// Elementary cellular automaton family? Awesome. Sometimes causes a
+// segmentation fault, not sure why.
+
+#include "../boiler/boiler.h"
+
+#include <bitset>
+#include <vector>
+#include <utility>
+#include <iostream>
+
+typedef unsigned char cell;
