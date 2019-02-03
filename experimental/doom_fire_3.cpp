e9ce3ad3a2af6290df8a405dae8fb693c5fe4223
CobaltXII <sidatt64@gmail.com>
2019-02-02 19:14:03 -0500

    Implemented struct point

---
+++ experimental/doom_fire_3.cpp
@@ -0,0 +1,46 @@
+/*
+
+Doom fire? Awesome.
+
+Thanks to Fabien Sanglard for his wonderful article:
+
+    http://fabiensanglard.net/doom_fire_psx/index.html
+
+Coloring algorithm from:
+
+	https://adasba.github.io/adasba-portfolio-site/cellular_automata/life_on_fire.html
+
+*/
+
+#include "../boiler/boiler.h"
+
+#include <vector>
+#include <utility>
+#include <iostream>
+
+struct point
+{
+	float x;
+	float y;
+
+	float ox;
+	float oy;
+
+	point(float _x, float _y)
+	{
+		x = _x;
+		y = _y;
+
+		ox = x;
+		oy = y;
+	}
+
+	point(float _x, float _y, float _ox, float _oy)
+	{
+		x = _x;
+		y = _y;
+
+		ox = _ox;
+		oy = _oy;
+	}
+};
