968c0fe20c7dcad9722cc5ab6400d84a578baedb
CobaltXII <sidatt64@gmail.com>
2019-02-08 21:07:51 -0500

    Implemented struct point with locked and anchor bools

--- experimental/verlet.cpp
+++ experimental/verlet.cpp
@@ -10,3 +10,71 @@
 // constraints. Take a look at constraint::do_constraint.
 
 bool jelly = false;
+
+// A point.
+
+struct point
+{
+	// Current position.
+
+	float x;
+	float y;
+
+	// Previous position.
+
+	float ox;
+	float oy;
+
+	// Locked points cannot move.
+
+	bool locked;
+
+	// Anchor points have a visible anchor.
+
+	bool anchor;
+
+	// Default constructor.
+
+	point
+	(
+		float _x,
+		float _y,
+
+		bool _locked = false,
+		bool _anchor = false
+	)
+	{
+		x = _x;
+		y = _y;
+
+		ox = x;
+		oy = y;
+
+		locked = _locked;
+		anchor = _anchor;
+	}
+
+	// Constructor, specify old position.
+
+	point
+	(
+		float _x,
+		float _y,
+
+		float _ox,
+		float _oy,
+
+		bool _locked = false,
+		bool _anchor = false
+	)
+	{
+		x = _x;
+		y = _y;
+
+		ox = _ox;
+		oy = _oy;
+
+		locked = _locked;
+		anchor = _anchor;
+	}
+};
