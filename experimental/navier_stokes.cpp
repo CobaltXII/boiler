d9eaf3f1d179b7f90b89eb109900291dc8ebf476
CobaltXII <sidatt64@gmail.com>
2019-02-09 18:03:57 -0500

    Added credits

---
+++ experimental/navier_stokes.cpp
@@ -0,0 +1,14 @@
+// Navier-Stokes fluid simulation? Awesome.
+//
+// Based on Daniel Shiffman's 2-dimensional fluid simulation:
+//
+//     https://github.com/shiffman/CFD/blob/master/CFDash/CFDash.pde
+//     https://github.com/shiffman/CFD/blob/master/CFDash/Fluid.pde
+//
+// which is based on Mike Ash's superb tutorial:
+//
+//     https://mikeash.com/pyblog/fluid-simulation-for-dummies.html
+//
+// which in turn is based on Jos Stam's spectacular paper:
+//
+//     http://www.dgp.toronto.edu/people/stam/reality/Research/pdf/GDC03.pdf
