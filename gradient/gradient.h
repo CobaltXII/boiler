b4dc48ed1d76cfabd498b0d024273e61dbf5fd24
CobaltXII <sidatt64@gmail.com>
2019-02-12 13:39:42 -0500

    Implemented rgb_tuple rgb_from_hex

--- gradient/gradient.h
+++ gradient/gradient.h
@@ -8,3 +8,39 @@ struct rgb_tuple
 	float g;
 	float b;
 };
+
+rgb_tuple rgb_from_hex(std::string hex)
+{
+	if (hex.size() != 6)
+	{
+		return {0.0f, 0.0f, 0.0f};
+	}
+
+	int raw_literals[6];
+
+	for (int j = 0; j < 6; j++)
+	{
+		for (int i = 0; i < 16; i++)
+		{
+			if ("0123456789abcdef"[i] == tolower(hex[j]))
+			{
+				raw_literals[j] = i;
+
+				break;
+			}
+		}
+	}
+
+	int combined_literals[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		combined_literals[i] = raw_literals[i * 2] * 16 + raw_literals[i * 2 + 1];
+	}
+
+	float r = combined_literals[0] / 255.0f;
+	float g = combined_literals[1] / 255.0f;
+	float b = combined_literals[2] / 255.0f;
+
+	return {r, g, b};
+}
