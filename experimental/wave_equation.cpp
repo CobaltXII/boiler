edce94bdd8b8a3c31a20147c01af6ffe757e130e
CobaltXII <sidatt64@gmail.com>
2019-02-12 15:11:22 -0500

    Added default constructor

--- experimental/wave_equation.cpp
+++ experimental/wave_equation.cpp
@@ -15,4 +15,15 @@ struct wave_equation
 
 	float* vx;
 	float* vy;
+
+	wave_equation(int x_res, int y_res)
+	{
+		this->x_res = x_res;
+		this->y_res = y_res;
+
+		values = (float*)malloc(x_res * y_res * sizeof(float));
+
+		vx = (float*)malloc(x_res * y_res * sizeof(float));
+		vy = (float*)malloc(x_res * y_res * sizeof(float));
+	}
 };
