79608c1cee055e5b5ef8984a7527e148364a88b8
CobaltXII <sidatt64@gmail.com>
2019-02-15 16:09:25 -0500

    Added OpenCL includes

--- experimental/n_body_cl/n_body_cl.cpp
+++ experimental/n_body_cl/n_body_cl.cpp
@@ -5,3 +5,17 @@
 #include <vector>
 #include <utility>
 #include <iostream>
+
+// Include OpenCL.
+
+#ifdef __APPLE__
+
+#define CL_SILENCE_DEPRECATION
+
+#include <OpenCL/OpenCL.h>
+
+#else
+
+#include <CL/cl.h>
+
+#endif
