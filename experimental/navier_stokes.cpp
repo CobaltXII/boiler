2c0634eb7562144df3d170cf89261b8e7c8cc3c6
CobaltXII <sidatt64@gmail.com>
2019-02-28 20:15:36 -0500

    Navier-Stokes simulation undergoes a major rehaul

--- experimental/navier_stokes.cpp
+++ experimental/navier_stokes.cpp
@@ -15,6 +15,8 @@
 
 #include "../boiler/boiler.h"
 
+#include "thermal_colormap.h"
+
 #include <vector>
 #include <utility>
 #include <iostream>
@@ -23,15 +25,12 @@
 
 struct game: boiler
 {
+	int x_res = 256;
+	int y_res = 256;
+
 	int scale = 3;
 
-	fluid simulation = fluid(256, 256, 0.01f, 1e-6f, 1e-9f);
-
-	int color = 7;
-
-	bool vorticity = true;
-
-	bool simulating = true;
+	fluid simulation = fluid(x_res, y_res, 1e-2f, 1e-5f, 1e-24f);
 
 	void steam() override
 	{
@@ -50,34 +49,6 @@ struct game: boiler
 		{
 			running = SDL_FALSE;
 		}
-		else if (key == SDLK_SPACE)
-		{
-			simulating = !simulating;
-		}
-		else if (key == SDLK_v)
-		{
-			vorticity = !vorticity;
-		}
-		else if (key == SDLK_r)
-		{
-			for (int i = 0; i < 16; i++)
-			{
-				int x = rand() % (simulation.x_res - 2) + 1;
-				int y = rand() % (simulation.y_res - 2) + 1;
-
-				simulation.add_density(x, y, 10239.0f);
-
-				float v_factor = 64.0f;
-
-				simulation.add_velocity
-				(
-					x, y, 
-
-					rand_11() * v_factor, 
-					rand_11() * v_factor
-				);
-			}
-		}
 		else if (key == SDLK_c)
 		{
 			for (int j = 0; j < simulation.y_res; j++)
@@ -89,34 +60,6 @@ struct game: boiler
 				simulation.density[simulation.idx(i, j)] = 0.0f;
 			}
 		}
-		else if (key == SDLK_1)
-		{
-			color = 0;
-		}
-		else if (key == SDLK_2)
-		{
-			color = 1;
-		}
-		else if (key == SDLK_3)
-		{
-			color = 2;
-		}
-		else if (key == SDLK_4)
-		{
-			color = 3;
-		}
-		else if (key == SDLK_5)
-		{
-			color = 4;
-		}
-		else if (key == SDLK_6)
-		{
-			color = 5;
-		}
-		else if (key == SDLK_7)
-		{
-			color = 6;
-		}
 	}
 
 	void draw() override
@@ -125,6 +68,8 @@ struct game: boiler
 
 		// Add some fluid.
 
+		float v_factor = 1e-1f;
+		
 		if (mouse_l || mouse_r)
 		{
 			// Calculate the delta mouse position.
@@ -132,41 +77,53 @@ struct game: boiler
 			int delta_x = mouse_x - mouse_ox;
 			int delta_y = mouse_y - mouse_oy;
 
-			if (mouse_l)
+			int r = 16;
+
+			for (int i = -r; i <= r; i++)
+			for (int j = -r; j <= r; j++)
 			{
-				simulation.add_density
-				(
-					mouse_x / scale,
-					mouse_y / scale,
+				float x = sqrtf(i * i + j * j) / float(r);
 
-					1024.0f
-				);
+				if (x < 1.0f)
+				{
+					float y = cosf(x * M_PI) + 1.0f;
+
+					if (mouse_l)
+					{
+						simulation.add_density
+						(
+							mouse_x / scale + i,
+							mouse_y / scale + j,
+
+							y * 16.0
+						);
+					}
+
+					y = y * y * y * y;
+
+					simulation.add_velocity
+					(
+						mouse_x / scale + i, 
+						mouse_y / scale + j,
+
+						y * delta_x * v_factor,
+						y * delta_y * v_factor
+					);
+				}
 			}
-
-			float v_factor = 2.0f;
-
-			simulation.add_velocity
-			(
-				mouse_x / scale, 
-				mouse_y / scale,
-
-				delta_x * v_factor,
-				delta_y * v_factor
-			);
 		}
 
-		if (simulating)
+		// Update the fluid.
+
+		simulation.step(1);
+
+		// Do vorticity confinement.
+
+		bool vorticity = true;
+
+		if (vorticity)
 		{
-			// Update the fluid.
-
-			simulation.step(1);
-
-			// Do vorticity confinement.
-
-			if (vorticity)
-			{
-				simulation.vorticity_confinement(10.0f);
-			}
+			simulation.vorticity_confinement(10.0f);
 		}
 
 		// Render the fluid.
@@ -174,66 +131,16 @@ struct game: boiler
 		for (int j = 1; j < simulation.y_res - 1; j++)
 		for (int i = 1; i < simulation.x_res - 1; i++)
 		{
-			if (color != 6)
-			{
-				unsigned char r = clamprgb(simulation.density[simulation.idx(i, j)] * 2);
+			frectrgb
+			(
+				(i - 1) * scale, 
+				(j - 1) * scale, 
 
-				unsigned char g = clamprgb(simulation.density[simulation.idx(i, j)] * 2 - 256);
-				unsigned char b = clamprgb(simulation.density[simulation.idx(i, j)] * 4 - 768);
+				scale, 
+				scale, 
 
-				unsigned int color_rgb;
-
-				if (color == 0)
-				{
-					color_rgb = rgb(r, g, b);
-				}
-				else if (color == 1)
-				{
-					color_rgb = rgb(r, b, g);
-				}
-				else if (color == 2)
-				{
-					color_rgb = rgb(g, r, b);
-				}
-				else if (color == 3)
-				{
-					color_rgb = rgb(g, b, r);
-				}
-				else if (color == 4)
-				{
-					color_rgb = rgb(b, r, g);
-				}
-				else if (color == 5)
-				{
-					color_rgb = rgb(b, g, r);
-				}
-
-				frectrgb
-				(
-					(i - 1) * scale, 
-					(j - 1) * scale, 
-
-					scale, 
-					scale, 
-
-					color_rgb
-				);
-			}
-			else
-			{
-				unsigned char grey = clamprgb(simulation.density[simulation.idx(i, j)]);
-
-				frectrgb
-				(
-					(i - 1) * scale, 
-					(j - 1) * scale, 
-
-					scale, 
-					scale, 
-
-					rgb(grey, grey, grey)
-				);
-			}
+				sample_thermal(clamprgb(simulation.density[simulation.idx(i, j)]))
+			);
 		}
 	}
 };
